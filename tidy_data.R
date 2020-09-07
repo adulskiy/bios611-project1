5f32925ec9a50d3893cd3460474bb329f9ef06c0
Anastasia Dulskiy <anadulskiy@Anastasias-MacBook-Pro.local>
2020-09-06 22:20:27 -0400

    Added notes about plans for tidying.

--- tidy_data.R
+++ tidy_data.R
@@ -6,7 +6,12 @@ stormradius <- read_csv("source_data/YIRCR12S_StormRadius.csv" );
 # Do something to clean stuff up.
 
 write_csv(growth, "derived_data/Growth.csv");
-write_csv(stormradius, "derived_data/StormRadius.csv");
+write_csv(stormradius, "derived_data/StormRadius.csv")
 
+str(growth)
+head(growth)
 
+# Stormradius - multiple data points per storm
+# Need to figure out how to handle this
+# Also need to convert m to km
 
